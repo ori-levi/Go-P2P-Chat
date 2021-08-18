0ed3fd42883702392bc140fbee81f2736aa3d7a0
Ori <none>
2021-08-18 15:39:37 +0300

    add infra for commands

--- client/client.go
+++ client/client.go
@@ -3,31 +3,81 @@ package client
 import (
 	"bufio"
 	"fmt"
+	"levi.ori/p2p-chat/common"
 	"net"
 	"net/textproto"
 	"os"
-	"time"
-
-	"levi.ori/p2p-chat/common"
+	"strings"
+	"sync"
 )
 
 var logger = common.NewLogger()
 
+var commands = map[string]func([]string){
+	"/pm":      pmCommand,
+	"/connect": connectCommand,
+	"/shell":   shellCommand,
+}
+
 type Client struct {
 	common.Client
 	Connection []net.Conn
+	reader     *bufio.Reader
+	locker     sync.RWMutex
 }
 
 func NewClient(name string) Client {
 	return Client{
 		Client:     common.NewClient(name, nil),
 		Connection: make([]net.Conn, 10),
+		reader:     bufio.NewReader(os.Stdin),
 	}
 }
 
-func (c *Client) Run(remoteUrl string, ic chan interface{}) {
-	c.handleConnection(remoteUrl, false)
-	time.Sleep(time.Hour)
+func (c *Client) Run(ic chan interface{}) {
+	//go c.handleInternalChannel(ic)
+
+	for {
+		fmt.Print(">>> ")
+		data, err := c.reader.ReadString('\n')
+		if err != nil {
+			logger.Fatalf("Failed to read from stdin, error: %v", err)
+		}
+
+		data = strings.Trim(data, " \n\r")
+		if strings.HasPrefix(data, "/") {
+			parts := strings.Split(data, " ")
+			command, arguments := parts[0], parts[1:]
+
+			if command == "/exit" {
+				break
+			}
+
+			if handler, ok := commands[command]; ok {
+				handler(arguments)
+			} else {
+				logger.Infof("Command %v is not recognized", command)
+			}
+		} else {
+			c.sendToAll(data)
+		}
+	}
+}
+
+func (c *Client) handleInternalChannel(ic chan interface{}) {
+	for {
+		rawMessage := <-ic
+		msg, ok := rawMessage.(common.InnerCommand)
+		if ok {
+			if msg.Command == common.ClientDisconnect {
+				client := msg.Data.(*common.Client)
+				logger.Debugf("client %v disconnected", client.RawConnection.RemoteAddr().String())
+			} else if msg.Command == common.ClientConnect {
+				client := msg.Data.(*common.Client)
+				logger.Debugf("client %v connect", client.RawConnection.RemoteAddr().String())
+			}
+		}
+	}
 }
 
 func (c *Client) MakeInternalConnection(serverPort int) {
@@ -45,11 +95,10 @@ func (c *Client) handleConnection(addr string, internal bool) error {
 		return err
 	}
 
-	reader := bufio.NewReader(os.Stdin)
 	for {
 		if len(c.Name) == 0 {
 			fmt.Print("Please enter your name: ")
-			c.Name, _ = reader.ReadString('\n')
+			c.Name, _ = c.reader.ReadString('\n')
 		}
 
 		id := register(conn, c.Name, internal)
@@ -63,7 +112,7 @@ func (c *Client) handleConnection(addr string, internal bool) error {
 		}
 
 		fmt.Printf("%v %v", code, message)
-		c.Name, _ = reader.ReadString('\n')
+		c.Name, _ = c.reader.ReadString('\n')
 	}
 
 	return nil
@@ -89,5 +138,26 @@ func (c *Client) Close() {
 		if err != nil {
 			logger.Errorf("Failed to close connection; %v", err)
 		}
+	}
+}
+
+func pmCommand(arguments []string) {
+	logger.Debugf("PM command %v", arguments)
+}
+
+func connectCommand(arguments []string) {
+	logger.Debugf("Connect command %v", arguments)
+}
+
+func shellCommand(arguments []string) {
+	logger.Debugf("Shell command %v", arguments)
+}
+
+func (c *Client) sendToAll(msg string) {
+	c.locker.RLock()
+	defer c.locker.RUnlock()
+
+	for _, conn := range c.Connection {
+		conn.Write([]byte(msg))
 	}
 }
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"flag"
+	"levi.ori/p2p-chat/common"
 
 	"levi.ori/p2p-chat/client"
-	"levi.ori/p2p-chat/common"
 	"levi.ori/p2p-chat/server"
 )
 
@@ -15,16 +15,13 @@ func main() {
 	var localServerPort int
 	flag.IntVar(&localServerPort, "server-port", server.DefaultPort, "local server port for listening")
 
-	var remoteUrl string
-	flag.StringVar(&remoteUrl, "remote", "", "remote server url (format: <ip>:<port>)")
-
 	var localInterfaceOnly bool
 	flag.BoolVar(&localInterfaceOnly, "local-iface", false, "listening only for local interface ("+server.InternalInterface+")")
 
 	flag.Parse()
 
-	if len(remoteUrl) == 0 {
-		common.Logger.Fatalf("Remote url is missing please run with -remote <ip>:<port>")
+	if len(name) == 0 {
+		common.Logger.Fatalf("Name is missing please run with -name <name>")
 	}
 
 	serverStartChannel := make(chan bool)
@@ -35,7 +32,7 @@ func main() {
 
 	clientApp := client.NewClient(name)
 	clientApp.MakeInternalConnection(localServerPort)
-	clientApp.Run(remoteUrl, serverApp.InternalClient.Channel)
+	clientApp.Run(serverApp.InternalClient.Channel)
 }
 
 func startServer(port int, internalIface bool, startChannel chan bool) *server.Server {
