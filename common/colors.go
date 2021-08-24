14aba603bb3bfd40fec06c793c788834ff875b95
Ori <none>
2021-08-24 15:02:17 +0300

    Fix ColorFprintln

--- common/colors.go
+++ common/colors.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -11,21 +12,21 @@ type Color string
 
 const (
 	ResetColor  Color = "\033[0m"
-	Black             = "\033[0;30m"
-	Red               = "\033[0;31m"
-	Green             = "\033[0;32m"
-	Gold              = "\033[0;33m"
-	Blue              = "\033[0;34m"
-	Purple            = "\033[0;35m"
-	Cyan              = "\033[0;36m"
-	LightGray         = "\033[0;37m"
-	DarkGray          = "\033[1;30m"
-	LightRed          = "\033[1;31m"
-	LightGreen        = "\033[1;32m"
-	Yellow            = "\033[1;33m"
-	LightBlue         = "\033[1;34m"
-	LightPurple       = "\033[1;35m"
-	LightCyan         = "\033[1;36m"
+	Black       Color = "\033[0;30m"
+	Red         Color = "\033[0;31m"
+	Green       Color = "\033[0;32m"
+	Gold        Color = "\033[0;33m"
+	Blue        Color = "\033[0;34m"
+	Purple      Color = "\033[0;35m"
+	Cyan        Color = "\033[0;36m"
+	LightGray   Color = "\033[0;37m"
+	DarkGray    Color = "\033[1;30m"
+	LightRed    Color = "\033[1;31m"
+	LightGreen  Color = "\033[1;32m"
+	Yellow      Color = "\033[1;33m"
+	LightBlue   Color = "\033[1;34m"
+	LightPurple Color = "\033[1;35m"
+	LightCyan   Color = "\033[1;36m"
 )
 
 var (
@@ -39,7 +40,12 @@ func ColorSprintf(c Color, format string, args ...interface{}) string {
 }
 
 func ColorFprintln(w io.Writer, c Color, a ...interface{}) (n int, err error) {
-	return fmt.Fprintln(w, c, a, ResetColor)
+	buff := []interface{}{c}
+	buff = append(buff, a...)
+	buff = append(buff, ResetColor)
+
+	str := fmt.Sprint(buff...)
+	return fmt.Fprintln(w, strings.Trim(str, " "))
 }
 
 func RandomColor() Color {
