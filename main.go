dad503043a4b02023f799fa5c47667842ea6b518
Ori <none>
2021-09-05 17:18:14 +0300

    create layout

--- main.go
+++ main.go
@@ -1,36 +1,6 @@
 package main
 
-import (
-	"flag"
-	"levi.ori/p2p-chat/client"
-	"levi.ori/p2p-chat/server"
-	"log"
-)
-
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "", "Your client name")
-
-	var port int
-	flag.IntVar(&port, "port", server.DefaultPort, "local server port for listening")
-
-	var localInterfaceOnly bool
-	flag.BoolVar(&localInterfaceOnly, "local-iface", false, "listening only for local interface ("+server.InternalInterface+")")
-
-	flag.Parse()
-
-	if len(name) == 0 {
-		log.Fatalln("Name is missing please run with -name <name>")
-	}
-
-	inputChannel := make(chan string)
-	logChannel := make(chan string)
-
-	serverApp := server.NewServer(name, port, localInterfaceOnly, logChannel)
-	go serverApp.RunServer()
-
-	clientApp := client.NewClient(name, port, logChannel)
-	go clientApp.Run(serverApp.InChannel, inputChannel)
-
-	uiMain(name, logChannel, serverApp.OutChannel, inputChannel)
+	w := newUi()
+	(*w).ShowAndRun()
 }
---
+++ ui.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+	"strings"
+)
+
+var titleStyle = fyne.TextStyle{
+	Bold:   true,
+	Italic: true,
+}
+
+func newUi() *fyne.Window {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	w.Resize(fyne.NewSize(1024, 768))
+
+	chatPanel, _ := newChatPanel()
+	usersPanel, _ := newUsersPanel()
+	notificationPanel, _ := newNotificationPanel()
+
+	//hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(
+		container.NewVBox(
+			container.NewBorder(
+				nil,
+				nil,
+				nil,
+				container.NewGridWithRows(2,
+					usersPanel,
+					newHelpPanel(),
+				),
+				chatPanel,
+			),
+			newInputPanel(),
+			notificationPanel,
+		),
+	)
+
+	return &w
+}
+
+func newInputPanel() *fyne.Container {
+	inputEntry := widget.NewEntry()
+	inputEntry.PlaceHolder = "What's On Your Mind?"
+
+	return container.NewBorder(
+		nil,
+		nil,
+		nil,
+		widget.NewButton("Send", func() {
+			fmt.Println(inputEntry.Text)
+			inputEntry.SetText("")
+		}),
+		inputEntry,
+	)
+}
+
+func newChatPanel() (*widget.Label, *string) {
+	chat := "chat"
+	l := widget.NewLabelWithData(binding.BindString(&chat))
+	return l, &chat
+}
+
+func newUsersPanel() (*fyne.Container, *string) {
+	users := "users"
+
+	w := container.NewVBox(
+		widget.NewLabelWithStyle("Users", fyne.TextAlignLeading, titleStyle),
+		widget.NewLabelWithData(binding.BindString(&users)),
+	)
+
+	return w, &users
+}
+
+func newHelpPanel() *fyne.Container {
+	help := []string{
+		fmt.Sprintf("%-9v <ip> <port>", "/connect"),
+		fmt.Sprintf("%-10v <name> <message...>", "/pm"),
+		fmt.Sprintf("%-10v <name> <command...>", "/shell"),
+		"/exit",
+	}
+
+	return container.NewVBox(
+		widget.NewLabelWithStyle("Help", fyne.TextAlignLeading, titleStyle),
+		widget.NewLabel(strings.Join(help, "\n")),
+	)
+}
+
+func newNotificationPanel() (*fyne.Container, *string) {
+	log := "logging"
+	w := container.NewVBox(
+		widget.NewLabelWithStyle("Notifications", fyne.TextAlignLeading, titleStyle),
+		widget.NewLabelWithData(binding.BindString(&log)),
+	)
+
+	return w, &log
+}
